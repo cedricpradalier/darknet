c78e0bfa4c42599158190e0ff8024310e7e4fc44
Cedric Pradalier <cedric.pradalier@georgiatech-metz.fr>
2021-07-13 08:23:59 +0000

    Modified stream capture to return NULL on error

--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -628,14 +628,14 @@ extern "C" void release_capture(cap_cv* cap)
 // ----------------------------------------
 
 extern "C" mat_cv* get_capture_frame_cv(cap_cv *cap) {
-    cv::Mat *mat = NULL;
     try {
-        mat = new cv::Mat();
         if (cap) {
             cv::VideoCapture &cpp_cap = *(cv::VideoCapture *)cap;
             if (cpp_cap.isOpened())
             {
+                cv::Mat *mat = new cv::Mat();
                 cpp_cap >> *mat;
+                return (mat_cv *)mat;
             }
             else std::cout << " Video-stream stopped! \n";
         }
@@ -644,7 +644,7 @@ extern "C" mat_cv* get_capture_frame_cv(cap_cv *cap) {
     catch (...) {
         std::cout << " OpenCV exception: Video-stream stoped! \n";
     }
-    return (mat_cv *)mat;
+    return NULL;
 }
 // ----------------------------------------
 
